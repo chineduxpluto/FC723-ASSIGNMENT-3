f83f7352d5559d758eea62a2ec3b1d3be459ed86
chineduxpluto <ezeaniobim@gmail.com>
2024-04-17 17:00:37 +0100

    defined a class to encapsulate the functionality of the algorithm

---
+++ encryption algorithm.py
@@ -0,0 +1,10 @@
+class EuclideanAlgorithm:
+    def __init__(self, a, b):
+        """
+        Initializes the Euclidean Algorithm with two integers
+        :param a: first integer
+        :param b: second integer
+        """
+        self.a = a
+        self.b = b
+

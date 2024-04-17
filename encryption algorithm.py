4f985e1e28fdfa79fa1622a8bf97f66f855d03c8
chineduxpluto <ezeaniobim@gmail.com>
2024-04-17 17:07:42 +0100

    defined a method to calculate gcd and a recursive helper method. Isolated the two methods for organizational purposes.

--- encryption algorithm.py
+++ encryption algorithm.py
@@ -8,3 +8,21 @@ class EuclideanAlgorithm:
         self.a = a
         self.b = b
 
+    def calculate_gcd(self):
+        """
+        Calculates the  greatest common divisor of the two integers using the recursive euclidean algorithm
+        :return: The greatest common divisor of the two integers
+        """
+        return self.gcd_recursive(self.a, self.b)
+
+    def gcd_recursive(self, a, b):
+        """
+        A helper method that computes the GCD of two integers using recursion
+        :param a: first integer
+        :param b: second integer
+        :return: the greatest common divisor (GCD) of a and b
+        """
+        if b == 0:
+            return a
+        else:
+            return self.gcd_recursive(b, a % b)

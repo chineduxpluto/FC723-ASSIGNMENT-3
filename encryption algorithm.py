46b5e3a6f24218111d5a0347ddb9f0d0a1bbf74f
chineduxpluto <ezeaniobim@gmail.com>
2024-04-17 18:03:31 +0100

    - added comments - improved clarity of Exception Handling - added second base case a == 0

--- encryption algorithm.py
+++ encryption algorithm.py
@@ -14,37 +14,39 @@ class EuclideanAlgorithm:
         while True:
             try:
                 value = int(input(number))
-                if value < 0:
+                if value < 0: #checks if value is greater than 0
                     raise ValueError("Please enter a positive integer.")
                 return value
-            except ValueError as e:
-                print(f"Invalid input: {e}")
+            except ValueError: #raises value error if number is not positive
+                print(f"Invalid input")
 
     def calculate_gcd(self):
         """
         Calculates the  greatest common divisor of the two integers using the recursive euclidean algorithm
         :return: The greatest common divisor of the two integers
         """
-        self.a = self.get_input("Enter the first positive number:  ")
-        self.b = self.get_input(("Enter the second positive number: "))
-        return self.gcd_recursive(self.a, self.b)
+        self.a = self.get_input("Enter the first positive number:  ") # fetch user input a from get_input method
+        self.b = self.get_input("Enter the second positive number: ") # fetch user input b from get_input method
+        return self.gcd_recursive(self.a, self.b) # compute the GCD using the recursive helper function
 
-    def gcd_recursive(self, a, b):
+    def gcd_recursive(self, a, b): # define helper method
         """
         A helper method that computes the GCD of two integers using recursion
         :param a: first integer
         :param b: second integer
         :return: the greatest common divisor (GCD) of a and b
         """
-        if b == 0:
-            return a
+        if b == 0: # first base case
+            return a # return a as GCD
+        if a == 0: # second base case
+            return b # return b as GCD
         else:
-            return self.gcd_recursive(b, a % b)
+            return self.gcd_recursive(b, a % b) # recursively apply the Euclidean Algorithm
 
 
 
 
 if __name__ == "__main__":
     gcd_calculator = EuclideanAlgorithm()  # Create an instance of the class
-    result = gcd_calculator.calculate_gcd()  # This method will handle input and calculation
-    print(f"The GCD is: {result}")
+    result = gcd_calculator.calculate_gcd()  # store result in a variable
+    print(f"The GCD is: {result}") # print result
